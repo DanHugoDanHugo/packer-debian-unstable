56888c96d117fc8e9ef550aa5a2e82d745e5ed8a
Dan Hugo <danielvhugo@gmail.com>
2016-08-22 12:05:51 -0700

    Add md5setup to download latest daily unstable MD5SUMS and fill in template.json with md5 sig

---
+++ md5setup.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+wget https://d-i.debian.org/daily-images/amd64/daily/MD5SUMS
+checksum=`grep netboot\/mini.iso MD5SUMS | cut -d' ' -f1`
+echo -n "Current published md5 checksum is "
+echo $checksum
+sed -i -e "s/^(.*)\"iso_checksum\"\:\ \".*\",$/$1\"iso_checksum\"\:\ \"$checksum\",/g" ./template.json
+
